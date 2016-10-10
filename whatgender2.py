0218774daae512c37371e2e254de9e624670a0c1
d0rh4m <azim.y@protonmail.com>
2016-10-10 13:53:34 +0800

    Update whatgender2.py
    Add comments

--- whatgender2.py
+++ whatgender2.py
@@ -1,21 +1,25 @@
+# User input variables 
 gender = (input("Would you consider yourself a male or a female? :" ))
 height = int(input("What is your height in cm: "))
 weight = int(input("What is your weight in kg: "))
 shoe_size = int(input("What is your shoe size in Euro: "))
 
+# Dependencies 
 from sklearn import tree
 from sklearn import neural_network
 from sklearn import discriminant_analysis
 from sklearn.metrics import accuracy_score
 
-#[height, weight, shoe size]
+# Training data: [height, weight, shoe size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
      [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43], [153, 53, 34]]
 
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'female', 'male', 'male', 'female']
 
+# Print user's input geder
 print (gender)
 
+# Move user input into data format 
 if gender == 'male':
 	gender_true = ['male']
 else: 
